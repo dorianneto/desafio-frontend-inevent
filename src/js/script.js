a6967a545967c8600e5217e12af3247342a322a4
Dorian Neto <doriansampaioneto@gmail.com>
2017-12-17 14:57:13 -0300

    feat(animations): add header fixed

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+var App = {
+    headerManipulation: function () {
+        var header = $('#header .navbar-fixed-top'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 100)
+            header.addClass('fixed');
+        else
+            header.removeClass('fixed');
+    }
+}
+
+$(window).scroll(function () {
+    App.headerManipulation();
+});
